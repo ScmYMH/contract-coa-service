0cda01d740959b1dc882712bc4719cc96cdf239b
DESKTOP-1Q3I6GN\BIT <mh.kim368@gmail.com>
2022-08-03 09:30:08 +0900

    coaservice

--- src/main/java/com/scm/contract/coa/service/CoaService.java
+++ src/main/java/com/scm/contract/coa/service/CoaService.java
@@ -14,35 +14,6 @@ import java.util.List;
 @Slf4j
 public class CoaService implements CoaServiceImpl{
 
-//    public List<ContractInfoEntity> getAllContractList() {
-//        return coaRepository.findAll();
-//    }
-
-//    public Stream<ContractInfoDto> getContractList(String cntrtId, String cntrtNm, String cntrtScd, String insDate){
-//        List<ContractInfoEntity> contractInfo = coaRepository.findByCntrtIdContainingAndCntrtIdContainingAndCntrtScdContainingAndInsDateContaining(cntrtId, cntrtNm, cntrtScd, insDate);
-//
-//        ContractInfoDto contractInfoDto = new ContractInfoDto();
-//
-//        Stream<ContractInfoDto> contractInfoData = contractInfo.stream().map(data -> {
-//            contractInfoDto.setCntrtId(data.getCntrtId());
-//            contractInfoDto.setCntrtTcd(data.getCntrtTcd());
-//            contractInfoDto.setCntrtNm(data.getCntrtNm());
-//            contractInfoDto.setCntrtScd(data.getCntrtScd());
-//            contractInfoDto.setCrePersonId(data.getCrePersonId());
-//            contractInfoDto.setCntrtStartDate(data.getCntrtStartDate());
-//            contractInfoDto.setCntrtEndDate(data.getCntrtEndDate());
-//            contractInfoDto.setCntrtCurrCd(data.getCntrtCurrCd());
-//            contractInfoDto.setDelYn(data.getDelYn());
-//            contractInfoDto.setInsDate(data.getInsDate());
-//            contractInfoDto.setInsTime(data.getInsTime());
-//            contractInfoDto.setInsPersonId(data.getInsPersonId());
-//            contractInfoDto.setUpdDate(data.getUpdDate());
-//            contractInfoDto.setUpdTime(data.getUpdTime());
-//            contractInfoDto.setUpdPersonId(data.getUpdPersonId());
-//            return contractInfoDto;
-//        });
-//        return contractInfoData;
-//    }
     @Autowired
     CoaMapper coaMapper;
 
