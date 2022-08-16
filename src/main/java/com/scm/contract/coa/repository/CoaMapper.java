5acd901712dc99601cdc1959ee33332537a47b5c
DESKTOP-1Q3I6GN\BIT <mh.kim368@gmail.com>
2022-08-16 21:05:42 +0900

    coa mapper

--- src/main/java/com/scm/contract/coa/repository/CoaMapper.java
+++ src/main/java/com/scm/contract/coa/repository/CoaMapper.java
@@ -13,11 +13,9 @@ public interface CoaMapper {
     List<ContractInfoDto> getContractInfo();
     List<ContractAndMasterJoinDto> getContractInfoByConditions(String cntrt_id, String cntrt_nm, String ins_date, String cd_v_meaning);
     List<TariffInfoDto> getTariffInfoByCntrtId(String cntrt_id, String svc_nm, String detl_svc_nm);
-
     List<CodeDefinitionDto> getCodeDefinition();
     String getAutoIncrementContractId();
-
     boolean postContractInfo(ContractInfoDto contractInfoDto);
-
     Integer updateCntrtInfo(ContractInfoDto contractInfoDto);
+    Integer deleteCntrtInfo(ContractInfoDto contractInfoDto);
 }
