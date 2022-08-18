53823b38eda4f1b68f75b0352536fffc8c799832
hyeja <21800799@handong.edu>
2022-08-18 12:07:50 +0900

    엑셀 임포트 validation 기능 추가

--- src/main/java/com/scm/contract/excel/service/ImportExcelServiceImpl.java
+++ src/main/java/com/scm/contract/excel/service/ImportExcelServiceImpl.java
@@ -2,14 +2,18 @@ package com.scm.contract.excel.service;
 
 import com.scm.contract.excel.model.ImportExcelDto;
 import com.scm.contract.excel.repository.ImportExcelMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
+@Slf4j
 public class ImportExcelServiceImpl implements ImportExcelService {
 
     @Autowired
@@ -51,39 +55,17 @@ public class ImportExcelServiceImpl implements ImportExcelService {
         importExcelDto.forEach(importExcelDto1 -> importExcelDto1.setUpd_person_id("202207130004"));
         importExcelDto.forEach(importExcelDto1 -> importExcelDto1.setLcc_cd_desc(importExcelDto1.getLcc_cd_desc()));
 
-//        importExcelDto.setCntrt_id("20220501000003");
-//        importExcelDto.setDel_yn("N");
-//        importExcelDto.setIns_date(new SimpleDateFormat("yyyyMMdd").format(today));
-//        importExcelDto.setIns_time(new SimpleDateFormat("HHmmss").format(today));
-//        importExcelDto.setIns_person_id("202207130004");
-//        importExcelDto.setArr_cd(importExcelDto.getArr_cd());
-//        importExcelDto.setArr_nm(importExcelDto.getArr_nm());
-//        importExcelDto.setDep_cd(importExcelDto.getDep_cd());
-//        importExcelDto.setDep_nm(importExcelDto.getDep_nm());
-//        importExcelDto.setProd_gcd(importExcelDto.getProd_gcd());
-//        importExcelDto.setInco_cd(importExcelDto.getInco_cd());
-//        importExcelDto.setPay_curr_cd(importExcelDto.getPay_curr_cd());
-//        importExcelDto.setCntrt_curr_cd(importExcelDto.getCntrt_curr_cd());
-//        importExcelDto.setTrff_end_date(importExcelDto.getTrff_end_date());
-//        importExcelDto.setTrff_stat_date(importExcelDto.getTrff_stat_date());
-//        importExcelDto.setSub_lcc_cd(importExcelDto.getSub_lcc_cd());
-//        importExcelDto.setLcc_cd(importExcelDto.getLcc_cd());
-//        importExcelDto.setTrff_id(271313);
-//        importExcelDto.setVal_seq_no(1);
-//        importExcelDto.setCond_id(importExcelDto.getCond_id());
-//        importExcelDto.setCond_nm(importExcelDto.getCond_nm());
-//        importExcelDto.setCal_unit_cd(importExcelDto.getCal_unit_cd());
-//        importExcelDto.setUnit_price(importExcelDto.getUnit_price());
-//        importExcelDto.setCorp_id("PI");
-//        importExcelDto.setUpd_date(new SimpleDateFormat("yyyyMMdd").format(today));
-//        importExcelDto.setUpd_time(new SimpleDateFormat("HHmmss").format(today));
-//        importExcelDto.setUpd_person_id("202207130004");
-//        importExcelDto.setLcc_cd_desc(importExcelDto.getLcc_cd_desc());
+        Integer aaa = importExcelMapper.excelValidationInfo(importExcelDto);
+        log.info("SELECT 카운트 값 >>> " + aaa);
+
+        if(aaa == 0) {
+            importExcelMapper.postImportExcelData(importExcelDto);
+            importExcelMapper.postImportExcelData2(importExcelDto);
+            return 0;
+        } else {
+            return 1;
+        }
 
 
-        importExcelMapper.postImportExcelData(importExcelDto);
-        importExcelMapper.postImportExcelData2(importExcelDto);
-
-        return null;
     }
 }
