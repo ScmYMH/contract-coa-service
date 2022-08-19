4c63c1861c0d689b2a0e543af394f7b44dde9103
DESKTOP-1Q3I6GN\BIT <mh.kim368@gmail.com>
2022-08-19 17:22:22 +0900

    coa

--- src/main/java/com/scm/contract/ContractApplication.java
+++ src/main/java/com/scm/contract/ContractApplication.java
@@ -2,12 +2,13 @@ package com.scm.contract;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ContractApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ContractApplication.class, args);
 	}
-
 }
