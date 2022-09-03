12c978ec26b7ff69eefabc24c2bfc9e63235510c
DESKTOP-1Q3I6GN\BIT <mh.kim368@gmail.com>
2022-09-03 15:39:47 +0900

    api change

--- src/main/java/com/scm/contract/coaChange/controller/ContractChangeController.java
+++ src/main/java/com/scm/contract/coaChange/controller/ContractChangeController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("coaChange")
+@RequestMapping("coa/change")
 @Slf4j
 public class ContractChangeController {
 
